bfa20924923f25b8b8ddf7f057b47bafd3a10cb7
mikabele <mikabele12@gmail.com>
2022-03-03 11:04:27 +0300

    add process func

--- src/main/scala/TableParser.scala
+++ src/main/scala/TableParser.scala
@@ -1,10 +1,23 @@
+import java.io.PrintWriter
+
 object TableParser {
-  case class CellParser(rowCnt:Int,colCnt:Int) {
-    def parseCellNum(cellNum:String):(Int,Int)={
-      val (word,num) = cellNum.split()
+  case class CellParser(rowCnt: Int, colCnt: Int) {
+    def parseCellNum(cellNum: String): (Int, Int) = {
+      val (word, num) = cellNum.split()
     }
   }
-  class Cell(rowCnt:Int,colCnt:Int,cellNum:String,val expr:String) {
-    val (row,col) = parseCellNum
+  class Cell(rowCnt: Int, colCnt: Int, cellNum: String, val expr: String) {
+    val (row, col) = parseCellNum
+  }
+
+  def calculateTableValues(inputFile: String): List[String] = {}
+
+  def process(line: String): Unit = {
+    line.split("\\s+").toList match {
+      case "--input" :: inputFilePath :: "--output" :: outputFilePath => {
+        val out = new PrintWriter(outputFilePath, "UTF-8")
+        calculateTableValues(inputFilePath).foreach()
+      }
+    }
   }
 }
