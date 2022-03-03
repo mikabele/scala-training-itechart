bf3cbadb211a8598fad8410f542502ab63e85cae
mikabele <mikabele12@gmail.com>
2022-03-03 11:09:56 +0300

    reformat code and add corner test cases

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -3,16 +3,14 @@ import scala.annotation.tailrec
 object Calculator {
   def lcm(a: Int, b: Int): Option[Int] = {
     gcd(a, b) match {
-      case 0 => None
-      case calcGCD => Some(Math.abs(a / calcGCD * b))
+      case 0   => None
+      case gcd => Some(Math.abs(a / gcd * b))
     }
   }
 
   @tailrec
   def gcd(a: Int, b: Int): Int = {
-    if (b == 0)
-      a
-    else
-      gcd(b, a % b)
+    if (b == 0) a
+    else gcd(b, a % b)
   }
 }
--- src/test/scala/CalculatorTest.scala
+++ src/test/scala/CalculatorTest.scala
@@ -1,19 +1,25 @@
 import org.scalatest.funsuite.AnyFunSuite
 
-class CalculatorTest extends AnyFunSuite{
-  test("Test gcd when a greater than b - successful path"){
-    assert(Calculator.gcd(105,252)==21)
+class CalculatorTest extends AnyFunSuite {
+  test("Test gcd when a greater than b - successful path") {
+    assert(Calculator.gcd(105, 252) == 21)
   }
-  test("Test gcd when a or b equals to 0"){
-    assert(Calculator.gcd(252,0)==252)
+  test("Test gcd when a or b equals to 0") {
+    assert(Calculator.gcd(252, 0) == 252)
   }
-  test("Test lcm when a greater than b - successful path"){
-    assert(Calculator.lcm(10,25).getOrElse(0)==50)
+  test("Test gcd with one negative number") {
+    assert(Calculator.gcd(20, -8) == 4)
   }
-  test("Test lcm when b or a equals to 0"){
-    assert(Calculator.lcm(25,0).getOrElse(-1)==0)
+  test("Test lcm when a greater than b - successful path") {
+    assert(Calculator.lcm(10, 25).getOrElse(0) == 50)
   }
-  test("Test lcm when a and b equal to 0"){
-    assert(Calculator.lcm(0,0).isEmpty)
+  test("Test lcm when b or a equals to 0") {
+    assert(Calculator.lcm(25, 0).getOrElse(-1) == 0)
+  }
+  test("Test lcm when a and b equal to 0") {
+    assert(Calculator.lcm(0, 0).isEmpty)
+  }
+  test("Test lcm with one negative number") {
+    assert(Calculator.lcm(20, -8).getOrElse(0) == 40)
   }
 }
