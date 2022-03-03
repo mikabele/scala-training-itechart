e376afbb7d6b42e1ca13ecb6f463c2a10a9f1248
mikabele <mikabele12@gmail.com>
2022-03-03 10:44:21 +0300

    updates after review, add scala formatter

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,15 +1,16 @@
 import scala.annotation.tailrec
 
 object Calculator {
-  def lcm(a: Int, b: Int): Int = {
-    a / gcd(a, b) * b
+  def lcm(a: Int, b: Int): Option[Int] = {
+    gcd(a, b) match {
+      case 0 => None
+      case calcGCD => Some(Math.abs(a / calcGCD * b))
+    }
   }
 
   @tailrec
   def gcd(a: Int, b: Int): Int = {
-    if (a == 0)
-      b
-    else if (b == 0)
+    if (b == 0)
       a
     else
       gcd(b, a % b)
--- src/test/scala/CalculatorTest.scala
+++ src/test/scala/CalculatorTest.scala
@@ -1,16 +1,19 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class CalculatorTest extends AnyFunSuite{
-  test("Test gcd when a greater than b"){
+  test("Test gcd when a greater than b - successful path"){
     assert(Calculator.gcd(105,252)==21)
   }
-  test("Test gcd when b greater than a"){
-    assert(Calculator.gcd(252,105)==21)
+  test("Test gcd when a or b equals to 0"){
+    assert(Calculator.gcd(252,0)==252)
   }
-  test("Test lcm when a greater than b"){
-    assert(Calculator.lcm(10,25)==50)
+  test("Test lcm when a greater than b - successful path"){
+    assert(Calculator.lcm(10,25).getOrElse(0)==50)
   }
-  test("Test lcm when b greater than a"){
-    assert(Calculator.lcm(25,10)==50)
+  test("Test lcm when b or a equals to 0"){
+    assert(Calculator.lcm(25,0).getOrElse(-1)==0)
+  }
+  test("Test lcm when a and b equal to 0"){
+    assert(Calculator.lcm(0,0).isEmpty)
   }
 }
