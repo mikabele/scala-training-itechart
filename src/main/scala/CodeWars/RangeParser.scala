43538f9cb6343ff9553f5859d24896a5e95bede2
mikabele <mikabele12@gmail.com>
2022-03-09 13:52:12 +0300

    split function in RangeParser

--- src/main/scala/CodeWars/RangeParser.scala
+++ src/main/scala/CodeWars/RangeParser.scala
@@ -13,24 +13,26 @@ object RangeParser {
     case "-" => dashFunc(strVal)
   }
 
+  private def addNumberToRangeStr(left: (String, Int), right: (String, Int)): (String, Int) = {
+    val (leftStrVal, leftNumValue) = left
+    val (_, rightNumValue)         = right
+    val newStr = rightNumValue - leftNumValue match {
+      case 1 =>
+        parseRes(leftStrVal)(x => x.dropRight(1) + ":", x => x.dropRight(1) + "-", x => x)
+      case _ =>
+        parseRes(leftStrVal)(
+          x => x + rightNumValue.toString + ",",
+          x => x.dropRight(1) + "," + leftNumValue.toString + "," + rightNumValue.toString + ",",
+          x => x + leftNumValue.toString + "," + rightNumValue.toString + ","
+        )
+    }
+    (newStr, rightNumValue)
+  }
+
   private def solutionNonEmpty(xs: List[Int]): String = {
     val (res, lastNum) = xs.tail
       .map(x => (x.toString, x))
-      .foldLeft((xs.head.toString + ",", xs.head))((left, right) => {
-        val (leftStrVal, leftNumValue) = left
-        val (_, rightNumValue)         = right
-        val newStr = rightNumValue - leftNumValue match {
-          case 1 =>
-            parseRes(leftStrVal)(x => x.dropRight(1) + ":", x => x.dropRight(1) + "-", x => x)
-          case _ =>
-            parseRes(leftStrVal)(
-              x => x + rightNumValue.toString + ",",
-              x => x.dropRight(1) + "," + leftNumValue.toString + "," + rightNumValue.toString + ",",
-              x => x + leftNumValue.toString + "," + rightNumValue.toString + ","
-            )
-        }
-        (newStr, rightNumValue)
-      })
+      .foldLeft((xs.head.toString + ",", xs.head))(addNumberToRangeStr)
     parseRes(res)(x => x.dropRight(1), x => x.dropRight(1) + "," + lastNum.toString, x => x + lastNum.toString)
   }
 
