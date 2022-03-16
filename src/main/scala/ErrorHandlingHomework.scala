c260b6ae084b553dd23818028c5139051429ccba
mikabele <mikabele12@gmail.com>
2022-03-16 12:09:30 +0300

    add dto classes, change logic

--- src/main/scala/ErrorHandlingHomework.scala
+++ src/main/scala/ErrorHandlingHomework.scala
@@ -1,52 +1,59 @@
 import ErrorHandlingHomework.AccountValidationError._
-import cats.data.Validated.{Invalid, Valid}
 import cats.data.ValidatedNec
 import cats.syntax.all._
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.auto._
+import eu.timepit.refined._
+import eu.timepit.refined.api.{Refined, Validate}
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.string._
 
 import java.text.SimpleDateFormat
-import java.time.{LocalDate, Period, ZoneId}
-import java.util.{Calendar, Date}
+import java.util.Calendar
 
 object ErrorHandlingHomework {
 
-  type PositiveInt    = Int Refined Positive
-  type BirthDay       = String Refined MatchesRegex["\\d{4}-\\d{2}-\\d{2}"]
-  type CardNumber     = String Refined MatchesRegex["\\d{4} \\d{4} \\d{4} \\d{4}"]
-  type ExpirationDate = String Refined MatchesRegex["\\d\\d/\\d\\d"]
-  type SecurityCode   = String Refined MatchesRegex["\\d{3}"]
+  type Age            = Interval.Open[0, 130]
+  type BirthDay       = MatchesRegex["\\d{4}-\\d{2}-\\d{2}"]
+  type CardNumber     = MatchesRegex["\\d{4} \\d{4} \\d{4} \\d{4}"]
+  type ExpirationDate = MatchesRegex["\\d\\d/\\d\\d"]
+  type SecurityCode   = MatchesRegex["\\d{3}"]
+  type PersonName     = MatchesRegex["[A-Z]+ [A-Z]+"]
+  type PassportNumber = MatchesRegex["[A-Z]{2}\\d{7}"]
 
   // think about adding Refined integration here to provide type level validation
+  //domain classes
   final case class Account(person: Person, card: PaymentCard)
   final case class Person(
-    name:           String,
-    age:            PositiveInt,
-    birthDay:       BirthDay,
-    passportNumber: String
+    name:           String Refined PersonName,
+    age:            Int Refined Age,
+    birthDay:       String Refined BirthDay,
+    passportNumber: String Refined PassportNumber
   ) // name, age, birthDay, passportNumber
   final case class PaymentCard(
-    cardNumber:     CardNumber,
-    expirationDate: ExpirationDate,
-    cardHolder:     String,
-    securityCode:   SecurityCode
+    cardNumber:     String Refined CardNumber,
+    expirationDate: String Refined ExpirationDate,
+    cardHolder:     String Refined PersonName,
+    securityCode:   String Refined SecurityCode
   ) // card number, expirationDate, securityCode etc
+
+  //dto classes
+  final case class PersonDto(name: String, age: Int, birthDay: String, passportNumber: String)
+  final case class PaymentCardDto(cardNumber: String, expirationDate: String, cardHolder: String, securityCode: String)
 
   sealed trait AccountValidationError
   object AccountValidationError {
-    final case object PersonIsNotACardHolder extends AccountValidationError {
-      override def toString: String = "Person name doesn't correspond name of card holder."
+    final case object InvalidExpirationDate extends AccountValidationError {
+      override def toString: String =
+        "You should enter expiration date in format MM/yy and this date shouldn't be over."
     }
-    final case object ExpirationDateIsOver extends AccountValidationError {
-      override def toString: String = "Expiration date is over."
+    final case object InvalidAge extends AccountValidationError {
+      override def toString: String = "You should select age in diapason from 0 to 130."
     }
-    final case object MismatchOfAgeAndBirthdate extends AccountValidationError {
-      override def toString: String = "Given age doesn't correspond birthdate."
+    final case object InvalidBirthdate extends AccountValidationError {
+      override def toString: String =
+        "You should enter birthdate in format yyyy-MM-dd"
     }
     final case object BirthdateFromFuture extends AccountValidationError {
-      override def toString: String = "You can't specify birthdate greater than Date.Now"
+      override def toString: String = "You can't enter birthdate after Date.Now"
     }
     final case object InvalidPersonName extends AccountValidationError {
       override def toString: String = "You should enter name according to pattern \"<NAME> <SURNAME>\" ignore case"
@@ -54,8 +61,17 @@ object ErrorHandlingHomework {
     final case object InvalidPassportNumber extends AccountValidationError {
       override def toString: String = "You should enter passport number according to pattern \"AA0000000\""
     }
-    final case object ZeroSecurityCode extends AccountValidationError {
-      override def toString: String = "Invalid security code 000"
+    final case object InvalidSecurityCode extends AccountValidationError {
+      override def toString: String = "Security code should have format \"XXX\""
+    }
+    final case object InvalidCardNumber extends AccountValidationError {
+      override def toString: String = "You should enter card number according to pattern \"XXXX XXXX XXXX XXXX\""
+    }
+    final case object ExpirationDateIsOver extends AccountValidationError {
+      override def toString: String = "Expiration date is over"
+    }
+    final case object MismatchAgeAndBirthdate extends AccountValidationError {
+      override def toString: String = "Mismatch of age and years from your birthdate"
     }
   }
 
@@ -63,90 +79,87 @@ object ErrorHandlingHomework {
 
     type AllErrorsOr[A] = ValidatedNec[AccountValidationError, A]
 
-    private val expirationDateFormat = new SimpleDateFormat("MM/yyyy")
+    private val expirationDateFormat = new SimpleDateFormat("MM/yy")
     private val birthDateFormat      = new SimpleDateFormat("yyyy-MM-dd")
-
-    private val personNamePattern:     scala.util.matching.Regex = "(\\w+ \\w+)".r
-    private val passportNumberPattern: scala.util.matching.Regex = "([A-Z]{2}\\d{7})".r
 
     private val dateNow = Calendar.getInstance().getTime
 
-    private def toLocalDate(date: Date): LocalDate = {
-      date.toInstant.atZone(ZoneId.systemDefault()).toLocalDate
-    }
+    private def refinedValidation[V, R](
+      value: V,
+      error: AccountValidationError
+    )(
+      implicit validator: Validate[V, R]
+    ): AllErrorsOr[V Refined R] =
+      refineV[R](value)(validator).left.map(_ => error).toValidatedNec
 
-    private def calcDateDiff(first: LocalDate, second: LocalDate): Int = {
-      val period = Period.between(first, second)
-      Math.abs(period.getYears)
-    }
+    private def validateCardNumber(number: String): AllErrorsOr[String Refined CardNumber] =
+      refinedValidation(number, InvalidCardNumber)
 
-    private def validateCardNumber(number: CardNumber): AllErrorsOr[CardNumber] = number.validNec
+    private def validateSecurityCode(code: String): AllErrorsOr[String Refined SecurityCode] =
+      refinedValidation(code, InvalidSecurityCode)
 
-    private def validateSecurityCode(code: SecurityCode): AllErrorsOr[SecurityCode] = if (code.value != "000")
-      code.validNec
-    else
-      ZeroSecurityCode.invalidNec
+    private def validateCardHolder(cardHolder: String): AllErrorsOr[String Refined PersonName] =
+      refinedValidation(cardHolder, InvalidPersonName)
 
-    private def validateCardHolder(name: String, cardHolder: String): AllErrorsOr[String] =
-      if (name.compareToIgnoreCase(cardHolder) == 0)
-        cardHolder.validNec
-      else
-        PersonIsNotACardHolder.invalidNec
-
-    private def validateExpirationDate(expirationDate: ExpirationDate): AllErrorsOr[ExpirationDate] =
-      if (expirationDateFormat.parse(expirationDate).before(dateNow))
+    private def validateExpirationDate(expirationDate: String): AllErrorsOr[String Refined ExpirationDate] = {
+      def checkExpirationDateIsOver(
+        validatedExpirationDate: String Refined ExpirationDate
+      ): AllErrorsOr[String Refined ExpirationDate] = if (
+        expirationDateFormat.parse(validatedExpirationDate.value).before(dateNow)
+      )
         ExpirationDateIsOver.invalidNec
       else
-        expirationDate.validNec
+        validatedExpirationDate.validNec
 
-    private def validateAge(age: PositiveInt, birthDay: BirthDay): AllErrorsOr[PositiveInt] = {
-      if (calcDateDiff(toLocalDate(dateNow), toLocalDate(birthDateFormat.parse(birthDay.value))) == age.value)
-        age.validNec
-      else
-        MismatchOfAgeAndBirthdate.invalidNec
+      refinedValidation[String, ExpirationDate](expirationDate, InvalidExpirationDate).andThen(
+        checkExpirationDateIsOver
+      )
     }
 
-    private def validatePersonName(name: String): AllErrorsOr[String] = name match {
-      case personNamePattern(_) => name.validNec
-      case _                    => InvalidPersonName.invalidNec
-    }
+    private def validateAge(age: Int): AllErrorsOr[Int Refined Age] =
+      refinedValidation(age, InvalidAge)
 
-    private def validatePassportNumber(number: String): AllErrorsOr[String] = number match {
-      case passportNumberPattern(_) => number.validNec
-      case _                        => InvalidPassportNumber.invalidNec
-    }
+    private def validatePersonName(name: String): AllErrorsOr[String Refined PersonName] =
+      refinedValidation(name, InvalidPersonName)
+
+    private def validatePassportNumber(number: String): AllErrorsOr[String Refined PassportNumber] =
+      refinedValidation(number, InvalidPassportNumber)
 
     private def validateBirthDate(
-      birthDay: BirthDay
-    ): AllErrorsOr[BirthDay] = if (birthDateFormat.parse(birthDay.value).before(dateNow))
-      birthDay.validNec
-    else
-      BirthdateFromFuture.invalidNec
+      birthDay: String
+    ): AllErrorsOr[String Refined BirthDay] = {
+      def checkBirthDateIsNotFromFuture(
+        validatedBirthDay: String Refined BirthDay
+      ): AllErrorsOr[String Refined BirthDay] =
+        if (birthDateFormat.parse(validatedBirthDay.value).before(dateNow))
+          validatedBirthDay.validNec
+        else
+          BirthdateFromFuture.invalidNec
 
-    private def validatePerson(p: Person): AllErrorsOr[Person] = (
+      refinedValidation[String, BirthDay](birthDay, InvalidBirthdate).andThen(checkBirthDateIsNotFromFuture)
+    }
+
+    def validatePerson(p: PersonDto): AllErrorsOr[Person] = (
       validatePersonName(p.name),
-      validateAge(p.age, p.birthDay),
+      validateAge(p.age),
       validateBirthDate(p.birthDay),
       validatePassportNumber(p.passportNumber)
     ).mapN(Person)
 
-    private def validatePaymentCard(p: Person, c: PaymentCard): AllErrorsOr[PaymentCard] = (
+    def validatePaymentCard(c: PaymentCardDto): AllErrorsOr[PaymentCard] = (
       validateCardNumber(c.cardNumber),
       validateExpirationDate(c.expirationDate),
-      validateCardHolder(p.name, c.cardHolder),
+      validateCardHolder(c.cardHolder),
       validateSecurityCode(c.securityCode)
     ).mapN(PaymentCard)
 
-    def validate(person: Person, card: PaymentCard): AllErrorsOr[Account] =
-      (validatePerson(person), validatePaymentCard(person, card)).mapN(Account)
+    def validate(personDto: PersonDto, cardDto: PaymentCardDto): AllErrorsOr[Account] =
+      (validatePerson(personDto), validatePaymentCard(cardDto)).mapN(Account)
   }
 
   def main(args: Array[String]): Unit = {
-    val person = Person("MIKHAIL BIALEVICH", 20, "2002-01-24", "HB1234567")
-    val card   = PaymentCard("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
-    AccountValidator.validate(person, card) match {
-      case Valid(a)   => println(a)
-      case Invalid(e) => println(e)
-    }
+    val person = PersonDto("MIKHAIL BIALEVICH", 20, "2002-01-24", "HB1234567")
+    val card   = PaymentCardDto("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
+    println(AccountValidator.validate(person, card))
   }
 }
--- src/test/scala/ErrorHandlingHomeworkTest.scala
+++ src/test/scala/ErrorHandlingHomeworkTest.scala
@@ -1,5 +1,5 @@
 import ErrorHandlingHomework.AccountValidationError._
-import ErrorHandlingHomework.{AccountValidator, PaymentCard, Person}
+import ErrorHandlingHomework.{AccountValidator, PaymentCard, PaymentCardDto, Person, PersonDto}
 import cats.data.Validated.Invalid
 import cats.syntax.all._
 import eu.timepit.refined.auto._
@@ -31,20 +31,10 @@ class ErrorHandlingHomeworkTest extends AnyFunSpec {
   }
 
   describe("Account") {
-    it("should throw PersonIsNotACardHolder if card holder name doesn't correspond person name") {
-      val person = Person("MIKHAI BIALEVICH", 20, "2002-01-24", "HB1234567")
-      val card   = PaymentCard("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
-      val acc    = AccountValidator.validate(person, card)
-      assert(acc.isInvalid)
-      val errChain = acc match {
-        case Invalid(e) => e.toList
-      }
-      assert(errChain.contains(PersonIsNotACardHolder))
-    }
 
     it("should throw BirthdateFromFuture error if birthdate greater than Date.now") {
-      val person = Person("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
-      val card   = PaymentCard("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
+      val person = PersonDto("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
+      val card   = PaymentCardDto("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
       val acc    = AccountValidator.validate(person, card)
       assert(acc.isInvalid)
       val errChain = acc match {
@@ -53,20 +43,9 @@ class ErrorHandlingHomeworkTest extends AnyFunSpec {
       assert(errChain.contains(BirthdateFromFuture))
     }
 
-    it("should throw ZeroSecurityCode if security code equals to 000") {
-      val person = Person("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
-      val card   = PaymentCard("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "000")
-      val acc    = AccountValidator.validate(person, card)
-      assert(acc.isInvalid)
-      val errChain = acc match {
-        case Invalid(e) => e.toList
-      }
-      assert(errChain.contains(ZeroSecurityCode))
-    }
-
     it("should throw ExpirationDateIsOver error if expiration date is over") {
-      val person = Person("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
-      val card   = PaymentCard("1234 1234 1234 1234", "07/21", "MIKHAIL BIALEVICH", "111")
+      val person = PersonDto("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
+      val card   = PaymentCardDto("1234 1234 1234 1234", "07/21", "MIKHAIL BIALEVICH", "111")
       val acc    = AccountValidator.validate(person, card)
       assert(acc.isInvalid)
       val errChain = acc match {
@@ -75,15 +54,5 @@ class ErrorHandlingHomeworkTest extends AnyFunSpec {
       assert(errChain.contains(ExpirationDateIsOver))
     }
 
-    it("should throw MismatchOfAgeAndBirthdate error if age doesn't correspond birthdate") {
-      val person = Person("MIKHAI BIALEVICH", 20, "2023-01-24", "HB1234567")
-      val card   = PaymentCard("1234 1234 1234 1234", "07/23", "MIKHAIL BIALEVICH", "111")
-      val acc    = AccountValidator.validate(person, card)
-      assert(acc.isInvalid)
-      val errChain = acc match {
-        case Invalid(e) => e.toList
-      }
-      assert(errChain.contains(MismatchOfAgeAndBirthdate))
-    }
   }
 }
